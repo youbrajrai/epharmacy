647d12cf0c8554a43752020235a4f887e4d88ffc
Youbraj <youbrajrai79@gmail.com>
2022-08-19 18:50:54 +0545

    First commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('id');
+        
+            $table->string('first_name')->nullable();
+            $table->string('last_name');
+       
+            $table->string('mobile')->nullable();
+            $table->integer('user_type');
+      
+         
+            $table->boolean('status')->default(1)->comment('0: Unrenewed 1: Active');
+            $table->string('work_phone')->nullable();
+            $table->string('address_street')->nullable();
+            $table->string('address_city')-> nullable();
+            $table->string('address_state')->nullable();
+            $table->string('address_postal_code')->nullable();
+            $table->text('description')->nullable();
+         
+            $table->string('email');
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
